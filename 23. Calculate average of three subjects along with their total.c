be42d60816a908b3b7fa24a1a5a667f51b7fc8e6
Krit302302 <kritpatel302302@gmail.com>
2025-08-24 19:10:47 +0530

    Add files via upload

---
+++ 23. Calculate average of three subjects along with their total.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+float main(){
+float a,b,c;
+printf("ENTER MARKS IN 3 SUBJECTS");
+scanf("%f %f %f",&a,&b,&c);
+printf("The Total marks in 3 subjects are %f\n",a+b+c);
+printf("THE Average marks in 3 subjects are %f\n",(a+b+c)/3.0);
+return 0;
+}
+

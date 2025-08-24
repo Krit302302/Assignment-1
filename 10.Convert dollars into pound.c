8ca07885d70a546280ba5b513ef802ddc62bc229
Krit302302 <kritpatel302302@gmail.com>
2025-08-24 16:34:48 +0530

    Add files via upload

---
+++ 10.Convert dollars into pound.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+float a,b,c;
+printf("ENTER THE AMOUNT IN DOLLARS");
+scanf("%f",&a);
+b=a*48;
+printf("THE GIVEN AMOUNT IN RS IS %f\n",b);
+c=b/70;
+printf("THE GIVEN AMOUNT IN POUNDS IS %f\n",c);
+return 0;
+}
+

865b0f3be49ac7448c034f48cab98c1473dd6891
Krit302302 <kritpatel302302@gmail.com>
2025-08-24 18:02:58 +0530

    Add files via upload

---
+++ 20. Calculate area of a triangle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+float main(){
+float H,L;
+printf("ENTER THE HEIGHT AND LENGTH OF THE TRIANGLE");
+scanf("%f %f",&H,&L);
+printf("THE AREA OF TRIANGLE IS %.15f",H*L/2);
+return 0;
+}
+
+

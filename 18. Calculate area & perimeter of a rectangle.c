d253877dca861295a822302be0bfeb921ec37362
Krit302302 <kritpatel302302@gmail.com>
2025-08-24 17:48:59 +0530

    Add files via upload

---
+++ 18. Calculate area & perimeter of a rectangle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+float main(){
+float L,B;
+printf("ENTER THE SIDES OF THE RECTANGLE(first length then breadth)");
+scanf("%f %f",&L,&B);
+printf("THE PERIMETER OF RECTANGLE IS %f\n",2*(L+B));
+printf("THE AREA OF THE RECTANGLE IS %f\n",L*B);
+return 0;
+}

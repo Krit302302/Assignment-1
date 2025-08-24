e39342589ad237b7b8b517f8947527ffe6501c8b
Krit302302 <kritpatel302302@gmail.com>
2025-08-24 16:54:24 +0530

    Add files via upload

---
+++ 14. Convert celcius into Fahrenheit.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+float a,b;
+printf("ENTER THE NUMBER IN CELCIUS");
+scanf("%f",&a);
+b=a*9/5+32;
+printf("THE GIVEN NUMBER IN FAHRENHEIT IS %.15f",b);
+return 0;
+}
+
+

ae45d8aeae47e1840c92d1744e6043492db50844
Krit302302 <kritpatel302302@gmail.com>
2025-08-24 16:22:37 +0530

    Add files via upload

---
+++ 8. Convert dollars into Rs..c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+float a;
+printf("ENTER THE AMOUNT IN DOLLARS");
+scanf("%f",&a);
+printf("THE GIVEN AMOUNT IN RS IS %f",a*48);
+return 0;
+}
---
+++ 9. Convert Rs. into dollars.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+float a;
+printf("ENTER THE AMOUNT IN RS");
+scanf("%f",&a);
+printf("THE GIVEN AMOUNT IN DOLLARS IS %f",a/48);
+return 0;
+}
